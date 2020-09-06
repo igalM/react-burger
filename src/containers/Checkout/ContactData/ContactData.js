06b736e0ea0f3519aae3a41f2eb52e97977ee535
igalM <igalmm@gmail.com>
2020-09-06 11:34:37 +0300

    formik

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,175 +1,138 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import styles from './ContactData.module.scss';
-import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import axios from '../../../axios-orders';
-import Input from '../../../components/UI/Input/Input';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import * as actionsCreators from '../../../store/actions';
-import {connect} from 'react-redux';
-import {updateObject, checkValidity} from '../../../shared/utility';
+import { connect } from 'react-redux';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import { FormControl } from '@material-ui/core';
 
-class ContactData extends Component {
-    state = {
-        orderForm: {
-            name: {
-                value: '',
-                elementType: 'input',
-                elementConfig: {
-                    type: 'text',
-                    placeholder: 'Your Name'
-                },
-                validation: {
-                    required: true
-                },
-                valid: false,
-                touched: false
-            },
-            street: {
-                value: '',
-                elementType: 'input',
-                elementConfig: {
-                    type: 'text',
-                    placeholder: 'Your Street'
-                },
-                validation: {
-                    required: true
-                },
-                valid: false,
-                touched: false
-            },
-            zipCode: {
-                value: '',
-                elementType: 'input',
-                elementConfig: {
-                    type: 'text',
-                    placeholder: 'ZIP Code'
-                },
-                validation: {
-                    required: true,
-                    minLength: 5,
-                    maxLength: 5
-                },
-                valid: false,
-                touched: false
-            },
-            country: {
-                value: '',
-                elementType: 'input',
-                elementConfig: {
-                    type: 'text',
-                    placeholder: 'Your Country'
-                },
-                validation: {
-                    required: true
-                },
-                valid: false,
-                touched: false
-            },
-            email: {
-                value: '',
-                elementType: 'input',
-                elementConfig: {
-                    type: 'email',
-                    placeholder: 'Your Email'
-                },
-                validation: {
-                    required: true
-                },
-                valid: false,
-                touched: false
-            },
-            deliveryMethod: {
-                value: 'fastest',
-                elementType: 'select',
-                elementConfig: {
-                    options: [
-                        {value: 'fastest', displayValue: 'Fastest'},
-                        {value: 'cheapest', displayValue: 'Cheapest'}
-                    ]
-                },
-                validation: {},
-                valid: true
-            }
-        },
-        formIsValid: false
+const ContactData = ({ ingredients, totalPrice, userId, loading, token, onOrderBurger }) => {
+
+    let form = <div>
+        <h4 className={styles.Title}>Enter your contact data</h4>
+        <Formik
+            initialValues={{ name: '', street: '', zipCode: '', country: '', email: '', deliveryMethod: 'fastest' }}
+            onSubmit={(values) => {
+                const order = {
+                    ingredients: ingredients,
+                    price: totalPrice,
+                    date: new Date().toLocaleDateString('en-GB'),
+                    userId: userId,
+                    orderData: values
+                };
+                onOrderBurger(order, token);
+            }}
+            validationSchema={Yup.object().shape({
+                name: Yup.string()
+                    .required('Required'),
+                street: Yup.string()
+                    .required('Required'),
+                zipCode: Yup.string()
+                    .required('Required'),
+                country: Yup.string()
+                    .required('Required'),
+                email: Yup.string()
+                    .email('Must be a valid email')
+                    .required('Required'),
+                deliveryMethod: Yup.string()
+                    .required('Required'),
+            })}>
+            {({ values,
+                touched,
+                errors,
+                isSubmitting,
+                handleChange,
+                handleBlur,
+                handleSubmit
+            }) => (
+                    <form onSubmit={handleSubmit}>
+                        <TextField
+                            error={errors.name && touched.name}
+                            label="Your Name"
+                            name="name"
+                            value={values.name}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            helperText={(errors.name && touched.name) && errors.name}
+                            margin="normal"
+                        />
+                        <TextField
+                            error={errors.street && touched.street}
+                            label="Your Street"
+                            name="street"
+                            value={values.comment}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            helperText={(errors.street && touched.street) && errors.street}
+                            margin="normal"
+                        />
+                        <TextField
+                            error={errors.zipCode && touched.zipCode}
+                            label="ZIP Code"
+                            name="zipCode"
+                            value={values.zipCode}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            helperText={(errors.zipCode && touched.zipCode) && errors.zipCode}
+                            margin="normal"
+                        />
+                        <TextField
+                            error={errors.country && touched.country}
+                            label="Country"
+                            name="country"
+                            value={values.country}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            helperText={(errors.country && touched.country) && errors.country}
+                            margin="normal"
+                        />
+                        <TextField
+                            error={errors.email && touched.email}
+                            label="Your Email"
+                            name="email"
+                            value={values.email}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            helperText={(errors.email && touched.email) && errors.email}
+                            margin="normal"
+                        />
+                        <FormControl margin="normal">
+                            <Select
+                                className="select-box"
+                                name="deliveryMethod"
+                                value={values.deliveryMethod}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                error={errors.deliveryMethod && touched.deliveryMethod}>
+                                <MenuItem value={'fastest'}>Fastest</MenuItem>
+                                <MenuItem value={'cheapest'}>Cheapest</MenuItem>
+                            </Select>
+                        </FormControl>
+                        <Button type="submit"
+                            variant="contained"
+                            color="primary"
+                            disabled={isSubmitting}>ORDER</Button>
+                    </form>
+                )}
+        </Formik>
+    </div>
+
+    if (loading) {
+        form = <Spinner />
     }
 
-    postHandler = (e) => {
-        e.preventDefault();
-        const formData = {};
-        for (let el in this.state.orderForm) {
-            formData[el] = this.state.orderForm[el].value;
-        }
-        const order = {
-            ingredients: this.props.ingredients,
-            price: this.props.totalPrice,
-            date: new Date().toLocaleDateString('en-GB'),
-            userId: this.props.userId,
-            orderData: formData
-        };
-        this.props.onOrderBurger(order, this.props.token);
-    }
-
-    inputChangedHandler = (event, el) => {
-        const updatedElement = updateObject(this.state.orderForm[el], {
-            value: event.target.value,
-            valid: checkValidity(this.state.orderForm[el].validation, event.target.value),
-            touched: true
-        });
-        const updatedForm = updateObject(this.state.orderForm, {
-            [el]: updatedElement
-        });
-        let formIsValid = true;
-
-        for (let input in updatedForm) {
-            formIsValid = updatedForm[input].valid && formIsValid;
-        }
-        this.setState({orderForm: updatedForm, formIsValid: formIsValid});
-    }
-
-    render() {
-        const formElements = [];
-
-        for (let key in this.state.orderForm) {
-            formElements.push({
-                id: key,
-                config: this.state.orderForm[key]
-            });
-        }
-
-        let form = (
-            <form onSubmit={(e) => this.postHandler(e)}>
-                {formElements.map(el => {
-                    return <Input
-                        key={el.id}
-                        elementType={el.config.elementType}
-                        elementConfig={el.config.elementConfig}
-                        value={el.config.value}
-                        valid={!el.config.valid}
-                        shouldValidate={el.config.validation}
-                        touched={el.config.touched}
-                        changed={(event) => this.inputChangedHandler(event, el.id)}
-                    />
-                })}
-                <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
-            </form>
-        );
-
-        let title = <h4>Enter your contact data</h4>
-
-        if (this.props.loading) {
-            form = <Spinner/>
-            title = null;
-        }
-
-        return (
-            <div className={styles.ContactData}>
-                {title}
-                {form}
-            </div>
-        );
-    }
+    return (
+        <div className={styles.ContactData}>
+            {form}
+        </div>
+    );
 }
 
 const mapStateToProps = state => ({
